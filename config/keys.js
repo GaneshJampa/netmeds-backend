7dca39b4c4fb03552255edfb9f3aff3562fc38a5
ganesh036 <91380384+GaneshJampa@users.noreply.github.com>
2022-02-04 00:43:22 +0530

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
 // Hold application secret and configurations
 module.exports = {
-	mongoURI: 'mongodb+srv://ganesh:M7gydcURmm4iamJ@cluster0.9j6go.mongodb.net/netmeds-backend?retryWrites=true&w=majority',
+	mongoURI: 'mongodb+srv://ganesh:M7gydcURmm4iamJ@cluster0.9j6go.mongodb.net/netmedsDatabase?retryWrites=true&w=majority',
 	SECRET_KEY: 'ReactNetmedsClone',
 	JWT_EXPIRE: '12h'
 };
